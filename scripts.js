712c6bada6f1eb60ae2ec9fde9c2b783b7cd9ccc
Jrootexe <137057219+Jrootexe@users.noreply.github.com>
2023-12-13 16:51:05 -0500

    Get divs to highlight when they are hovered over

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 const container = document.querySelector('#container');
 
 for (i = 1; i < 257; i++) {
-    const squares = document.createElement('div');
-    container.appendChild(squares);
+    const div = document.createElement("div");
+    div.id = "squares";
+    container.appendChild(div);
 }
+
+const highlight = document.querySelectorAll("#squares");
+
+highlight.forEach(el => el.addEventListener("mouseover", function(){
+    (this).classList.toggle("active");
+}));
